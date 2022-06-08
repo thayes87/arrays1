b52c2ba9fd37476062db35bc940fff06eb1d9de9
Thomas Hayes <hayes.thomas610@gmail.com>
2022-06-08 10:46:36 -0600

    Initial commit

---
+++ array2.rb
@@ -0,0 +1,4 @@
+Bike_brands = ["trek", "specialized", "giant", "cannondale"]
+Bike_frame_sizes = [17, 19, 21, 23]
+Bike_wheel_sizes = [24.0, 26.0, 27.5, 29]
+Bike_purchase = [true, false, false, true]
