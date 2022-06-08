4066f4aa8764b24f05e165345464b3e5deee7dc9
Thomas Hayes <hayes.thomas610@gmail.com>
2022-06-08 10:57:57 -0600

    Add method and ruby comments

--- array2.rb
+++ array2.rb
@@ -2,3 +2,15 @@ Bike_brands = ["trek", "specialized", "giant", "cannondale"]
 Bike_frame_sizes = [17, 19, 21, 23]
 Bike_wheel_sizes = [24.0, 26.0, 27.5, 29]
 Bike_purchase = [true, false, false, true]
+
+#identifies last element from array and returns it
+Bike_purchase.pop
+
+#identifies last element from array and returns it
+Bike_wheel_sizes.shift
+
+#allows addition of and element to the end of the array
+Bike_brands.push("norco", "revel")
+
+#allows addition of an element as element[0]
+Bike_frame_sizes.unshift(15)
